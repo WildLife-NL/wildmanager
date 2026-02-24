4a4111f3e9280b8730d817b0f427afca9ce155c2
Guusvdz <515191@student.fontys.nl>
2026-02-24 16:09:29 +0100

    fix #5

--- wildmanager/packages/wildlifenl_detection_components/lib/src/interfaces/detection_read_api_interface.dart
+++
@@ -1,9 +0,0 @@
-abstract class DetectionReadApiInterface {
-  Future<List<Map<String, dynamic>>> getDetectionsByFilter({
-    required DateTime start,
-    required DateTime end,
-    required double latitude,
-    required double longitude,
-    required int radius,
-  });
-}
--- wildmanager/packages/wildlifenl_detection_components/lib/wildlifenl_detection_components.dart
+++
@@ -1,7 +0,0 @@
-library wildlifenl_detection_components;
-
-export 'src/interfaces/detection_read_api_interface.dart';
-export 'src/api/http_detection_read_api.dart';
-export 'src/models/detection_type.dart';
-export 'src/models/detection_pin_style.dart';
-export 'src/utils/detection_filters.dart';
