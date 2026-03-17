a18e274a1662683b25a088c9f8273bca89b9678c
Guusvdz <515191@student.fontys.nl>
2026-03-17 14:52:49 +0100

    fix #50

--- wildmanager/build/web/flutter_service_worker.js
+++ wildmanager/build/web/flutter_service_worker.js
@@ -141,7 +141,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a384a94e91aaf70a7b7ca0f103fca9aa
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "90d29e481fd7994be4598eb31f656506",
+"flutter_bootstrap.js": "1e83594ede06f7cbc4bfbea9e626175d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
